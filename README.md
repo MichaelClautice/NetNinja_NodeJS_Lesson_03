fc10c393a786d783ec1566e898609cc6f275026e
Michael Clautice <michaelclautice@Michaels-MacBook-Pro-2.local>
2021-05-07 12:40:56 -0400

    Mike modified 2 Lesson 03 files.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NetNinja_Lesson_03
 
-Michael_Clautice, Richmond, VA
+Michael Clautice, Richmond, VA
 The Net Ninja - Node JS Tutorial for Beginners 
 Lesson 03 - The V8 Engine
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// Michael_Clautice, Richmond, VA
+// Michael Clautice, Richmond, VA
 // The Net Ninja - Node JS Tutorial for Beginners
 // Lesson 03 - The V8 Engine
 //-------
 // below I output 3 lines in one console.log instead of 3 via \n line break - yay!
-console.log('\n•••••••\nHello Ninjas!\n••••••••');
+// extra line breaks = extra empty lines - yay!
+console.log('\n\n\n••••••\nHowdy Doody to all Ninjas!\n••••••••');
